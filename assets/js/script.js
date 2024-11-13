611b5592d030d796235e25e4dceeb628fe58acef
SannyGarin <141878054+SannyGarin@users.noreply.github.com>
2024-11-13 17:37:14 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Data Analytics", "Data Science", "Data Engineering"],
+    strings: ["Data Analytics", "Data Science", "Data Warehousing","Data Engineering"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
